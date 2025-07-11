5119b3b9a274bf6184c0b2d2e655e3d1e4cccedb
uhsr <uhsr@eteb.me>
2025-07-11 20:04:24 +0700

    Add vuecryptocoretechnext.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/vuecryptocoretechnext/vuecryptocoretechnext.go
@@ -0,0 +1,36 @@
+// internal/vuecryptocoretechnext/vuecryptocoretechnext.go
+package vuecryptocoretechnext
+
+import (
+"log"
+"os"
+)
+
+type App struct {
+verbose bool
+logger *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+app := &App{
+verbose: verbose,
+logger: log.New(os.Stdout, "", log.LstdFlags),
+}
+
+if verbose {
+app.logger.SetPrefix("[DEBUG] ")
+} else {
+app.logger.SetPrefix("[INFO] ")
+}
+
+return app
+}
+
+func (a *App) Run() error {
+a.logger.Printf("Starting %s processing", "VueCryptoCoreTechNext")
+
+// Add your implementation here
+
+a.logger.Println("Processing completed successfully")
+return nil
+}
